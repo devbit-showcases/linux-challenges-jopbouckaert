80b40b22efd5d2f413962d57ce3da9a31447d4b0
Jop Bouckaert <jop.bouckaert@student.vives.be>
2019-04-19 12:46:57 +0100

    update typo

--- rpi-script/dump.sh
+++ rpi-script/dump.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 touch ./output.txt
-sudo dhcpdump -i eth0 -h^b8:27:eb | mawk -W interactive '/OPTION:  50 / {print $8} /OPTION:  61 / {print $6} /TIME: / {print $2 " " $3}'  | tee ./output.txt
+sudo dhcpdump -i eth0 -h ^b8:27:eb | mawk -W interactive '/OPTION:  50 / {print $8} /OPTION:  61 / {print $6} /TIME: / {print $2 " " $3}'  | tee ./output.txt
