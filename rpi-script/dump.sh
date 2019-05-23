bdb80f2fb4d30fed11ca2b88f1f08750875bd092
Jop Bouckaert <jop.bouckaert@outlook.com>
2019-05-23 09:05:17 +0200

    refactor rpi-script

--- rpi-script/dump.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env bash
-touch ./output.txt
-sudo dhcpdump -i eth0 -h^b8:27:eb | mawk -W interactive '/OPTION:  50 / {print $8} /IP: / {gsub(/[()]/,"");print $3} /TIME: / {print $2 " " $3} /OPTION:  12 / {print $7}'  | tee ./output.txt
--- rpi-script/script.sh
+++ rpi-script/script.sh
@@ -1,4 +1,21 @@
-#!/usr/bin/env bash
-pkill -f ./file.sh
-./file.sh &
-./dump.sh > /dev/null 2>&1
+sudo dhcpdump -i eth0 -h ^b8:27:eb | while read dump; do
+    case $dump in
+     *TIME:*)     
+          time=$(echo $dump | mawk -W interactive '/TIME: / {print $2 " " $3}')
+          ;;
+     *IP:*)      
+          mac=$(echo $dump | mawk -W interactive '/IP: / {gsub(/[()]/,"");print $3}')
+          ;;
+     *OPTION:\ \ 50\ *)
+          ip=$(echo $dump | mawk -W interactive '{print $8} ')
+          ;; 
+     *OPTION:\ \ 12\ *)
+          hostname=$(echo $dump | mawk -W interactive '{print $7}')
+          ;;
+     *-----*)
+            echo "[${time}] ${mac} => ${ip} from ${hostname}" | tee ./devicelog.txt;
+            curl --header 'Content-Type: application/json' --request POST -d "{\"update\":{\"name\":\"${name}\",\"mac\":\"${mac}\",\"ip_address\":\"${ip}\"}}" http://mydevices.labict.xyz/updates.json 2> /dev/null >> ./curllog.txt
+          ;;
+     *)
+    esac
+done
